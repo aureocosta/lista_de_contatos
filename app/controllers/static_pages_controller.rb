e5f8c6efa3ea88455a4b80bc9ce56e641336eb2c
Áureo Costa <aureocosta.s@gmail.com>
2020-01-22 17:24:51 -0300

    criação do formulário

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,11 @@ class StaticPagesController < ApplicationController
     def index 
         @nome = "Hello world!" 
     end    
+    def sobre
+         
+    end 
+    def contato
+
+    end        
 
 end
---
+++ db/teste.rb
@@ -0,0 +1,3 @@
+def exibe soma (arg1,arg2)
+    print arg1 + arg2
+end 
